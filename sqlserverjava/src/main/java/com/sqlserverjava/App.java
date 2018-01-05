73540128b1ebe2c95218908de9abd27a616895b5
fretlesskey <jmsnavarro@gmail.com>
2018-01-05 20:48:33 +0800

    Initial commit

---
+++ sqlserverjava/src/main/java/com/sqlserverjava/App.java
@@ -0,0 +1,36 @@
+package com.sqlserverjava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+// To test:
+// 1.) build the project 
+//     $ mvn package
+// 2.) Run the package
+//     $ mvn -q exec:java "-Dexec.mainClass=com.<package_name>.App"
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// Build connection
+        String hostname = "centos.localsandbox.com";
+        String port = "1433";
+        String databaseName = "master";
+        String userName = "sa";
+        String password = "pAssw0rd123$";
+		String connectionUrl = "jdbc:sqlserver://" + hostname + ":" + port + ";databaseName=" + databaseName + ";user=" + userName + ";password=" + password;
+
+		try {
+			// Load SQL Server JDBC driver and establish connection.
+			System.out.print("Connecting to SQL Server ... ");
+			try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+				System.out.println("Done.");
+			}
+		} catch (Exception e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
+}
